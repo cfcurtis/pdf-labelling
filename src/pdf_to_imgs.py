dadb80749db9b8ee005e38664769bb5b2c31b2e7
Charlotte Curtis <ccurtis@mtroyal.ca>
2023-03-23 17:48:07 -0600

    Moving more things to common

--- src/pdf_to_imgs.py
+++ src/pdf_to_imgs.py
@@ -4,26 +4,6 @@ from pathlib import Path
 
 import common as c
 
-
-def write_svg(svg: str, filename: str) -> None:
-    """Writes the svg string to the given filename."""
-    with open(filename, "w") as f:
-        f.write(svg)
-
-
-def page_to_png(folder: Path, page: fitz.Page, layer_name: str) -> None:
-    """Saves a page as a .png"""
-    try:
-        pix = page.get_pixmap(dpi=c.DPI)
-        if pix.color_count() < 2:
-            # there's nothing on this page, skip it
-            print(f"Skipping page {page.number} {layer_name}, no content")
-        else:
-            pix.save(folder / f"{folder.stem}-p{page.number:02d}-{layer_name}.png")
-    except OSError as e:
-        print(e)
-
-
 def explode_pdf(doc: fitz.Document) -> None:
     """
     Creates a folder of images for each layer and page in the PDF.
@@ -40,7 +20,7 @@ def explode_pdf(doc: fitz.Document) -> None:
 
         # then save an image of all the rest of the layers
         for page in doc:
-            page_to_png(im_dir, page, "all_layers")
+            c.page_to_png(im_dir, page, "all_layers")
 
         # get the dictionary of layers
         ui_configs = doc.layer_ui_configs()
@@ -50,7 +30,7 @@ def explode_pdf(doc: fitz.Document) -> None:
 
         # save the background layer
         for page in doc:
-            page_to_png(im_dir, page, "background")
+            c.page_to_png(im_dir, page, "background")
 
         for layer in ui_configs:
             layer_name = layer["text"].replace(" ", "")
@@ -63,7 +43,7 @@ def explode_pdf(doc: fitz.Document) -> None:
 
             # write the current layer to an image
             for page in doc:
-                page_to_png(im_dir, page, layer_name)
+                c.page_to_png(im_dir, page, layer_name)
 
             # turn the current layer back off
             doc.set_layer_ui_config(layer["number"], action=2)
@@ -73,8 +53,11 @@ def explode_pdf(doc: fitz.Document) -> None:
 
 
 def main(pdf_path) -> None:
-    doc = fitz.open(pdf_path)
-    explode_pdf(doc)
+    try:
+        doc = fitz.open(pdf_path)
+        explode_pdf(doc)
+    except fitz.FileDataError:
+        print(f"Could not open {pdf_path}, perhaps it's not a PDF")
 
 
 if __name__ == "__main__":
